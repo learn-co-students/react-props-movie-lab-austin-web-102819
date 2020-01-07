8b032c5f7549d02bfbb4ca3544ec264f4e8f379f
BrittneyHaselmayer <brittneyhaselmayer@yahoo.com>
2020-01-07 15:43:05 -0600

    Done.

--- src/MovieShowcase.js
+++ src/MovieShowcase.js
@@ -5,7 +5,16 @@ import movieData from './data.js'
 export default class MovieShowcase extends Component {
 
   generateMovieCards = () => {
-    // map over your movieData array and return an array of the correct JSX
+        // map over your movieData array and return an array of the correct JSX
+        return movieData.map((movie, index) =>
+          <MovieCard
+              key={index}
+              title={movie.title}
+              IMDBRating={movie.IMDBRating}
+              genres={movie.genres}
+              poster={movie.poster}
+          />
+        )
   }
 
   render() {
@@ -16,3 +25,10 @@ export default class MovieShowcase extends Component {
     )
   }
 }
+
+
+
+
+
+
+
